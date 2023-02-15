5b34da422882d6cbf1dd2bb8274accee87a77df9
Tom-Ate <105049719+Tom-Ate@users.noreply.github.com>
2023-02-15 10:48:51 +0100

    Fachkonzept Klassen erstellt

---
+++ src/fachkonzept/VokDaten.java
@@ -0,0 +1,17 @@
+package fachkonzept;
+
+public class VokDaten
+{
+	private int VokabelNr;
+	private String vokabelE;
+	private String vokabelD;
+	private int KategorieNr; 
+	
+	public VokDaten(int pVokabelNr, String pVokabelE, String pVokabelD, int pKategorieNr)
+	{
+		VokabelNr = pVokabelNr;
+		vokabelE = pVokabelE;
+		vokabelD = pVokabelD;
+		KategorieNr = pKategorieNr;
+	}
+}
---
+++ src/fachkonzept/vokabeln.java
@@ -0,0 +1,33 @@
+package fachkonzept;
+
+import java.util.ArrayList;
+
+public class vokabeln
+{
+	private ArrayList<VokDaten>dieVokDaten;
+	public vokabeln()
+	{
+		dieVokDaten = new ArrayList<>();
+		// dieVokdaten = Arraylist die von Datenspeicherung übergeben wird 
+	}
+	
+	public <arraylist>VokDaten liesVokabeln()
+	{
+		
+	}
+	
+	public void fuegeHinzu(VokDaten pVokDaten)
+	{
+		
+	}
+	public void loescheVokabel(int pVokabelNr)
+	{
+		
+	}
+	public void bearbeiteVokabel(int pVokabelNr, VokDaten pVokDaten)
+	{
+		
+	}
+	
+	
+}
