dd8f5687a0583607465ee3245a6fa016691837c2
Tom-Ate <105049719+Tom-Ate@users.noreply.github.com>
2023-03-22 11:03:24 +0100

    calculate Error rate

---
+++ src/fachkonzept/Lerneinheit.java
@@ -0,0 +1,46 @@
+package fachkonzept;
+
+import java.util.ArrayList;
+
+public class Lerneinheit
+{
+	private ArrayList<VokDaten> dieAktuellenVokDaten;
+	public Lerneinheit()
+	{
+		dieAktuellenVokDaten = new ArrayList<>();
+	}
+	public double calculateErrorRate(int numCorrect, int numIncorrect) {
+	    double total = numCorrect + numIncorrect;
+	    if (total == 0) {
+	        return 0.0;
+	    }
+	    double errorRate = (double) numIncorrect / total;
+	    return errorRate;
+	} 
+	public ArrayList<VokDaten> randomizedArray(ArrayList<VokDaten>pVokDaten,int pMenge) throws Exception
+	{
+		
+		int PriortiyZusammen = 0;
+		if(pVokDaten.size()<pMenge)
+		{
+			return pVokDaten;
+		}
+		if(pVokDaten.isEmpty())
+		{
+			throw new Exception("Fick die Arraylist da is nix dinna");
+		}
+		for(int i = 0; i < pVokDaten.size(); i++)
+		{
+			PriortiyZusammen += pVokDaten.get(i).liesPrioritaet();
+		}
+		for(int i = 0; i<pMenge; i++)
+		{
+			dieAktuellenVokDaten.add(pVokDaten.get((int) Math.random()*pVokDaten.size()+1));
+		}
+		
+		
+		return dieAktuellenVokDaten;
+		
+	}
+}
+
--- src/fachkonzept/VokDaten.java
+++ src/fachkonzept/VokDaten.java
@@ -3,16 +3,21 @@ package fachkonzept;
 public class VokDaten
 {
 	private int VokabelNr;
+	private String sprache;
 	private String vokabelE;
 	private String vokabelD;
-	private int KategorieNr; 
+	private int KategorieNr;
+	private int prioritaet;
+
 	
-	public VokDaten(int pVokabelNr, String pVokabelE, String pVokabelD, int pKategorieNr)
+	public VokDaten(int pVokabelNr,String pSprache, String pVokabelE, String pVokabelD, int pKategorieNr, int pPrioritaet)
 	{
 		VokabelNr = pVokabelNr;
 		vokabelE = pVokabelE;
 		vokabelD = pVokabelD;
 		KategorieNr = pKategorieNr;
+		sprache = pSprache;
+		prioritaet = pPrioritaet;
 	}
 	
 	public int liesVokabelNr()
@@ -31,4 +36,13 @@ public class VokDaten
 	{
 		return KategorieNr;
 	}
+	public String liesSprache()
+	{
+		return sprache;
+	}
+	public int liesPrioritaet()
+	{
+		return prioritaet;
+	}
 }
+
